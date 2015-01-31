4093a5b6a93e6679a7797d27d0fb7e24fc47aae3
Alexander Müller <alexander.mueller@codecentric.de>
2015-01-31 18:54:45 +0100

    Issue #18: It is now possible to reuse variable-aware statements with different values within a single execution.

--- zucchini-examples/src/test/java/de/codecentric/zucchini/examples/VariablesTest.java
+++ zucchini-examples/src/test/java/de/codecentric/zucchini/examples/VariablesTest.java
@@ -32,69 +32,86 @@ import static de.codecentric.zucchini.bdd.dsl.impl.ScenarioBuilder.given;
 import static de.codecentric.zucchini.bdd.vars.Variables.charSequenceVar;
 import static de.codecentric.zucchini.bdd.vars.Variables.stringVar;
 import static de.codecentric.zucchini.web.facts.WebFacts.onPage;
+import static de.codecentric.zucchini.web.pageobjects.SimplePage.url;
 import static de.codecentric.zucchini.web.results.WebResults.see;
-import static de.codecentric.zucchini.web.steps.WebSteps.submit;
-import static de.codecentric.zucchini.web.steps.WebSteps.type;
+import static de.codecentric.zucchini.web.steps.WebSteps.*;
 import static de.codecentric.zucchini.web.vars.ParameterizedBy.element;
 import static de.codecentric.zucchini.web.vars.WebVariables.pageVar;
 
 public class VariablesTest {
-    @Rule
-    public WebDriverExecutorRule webDriverExecutorRule = new WebDriverExecutorRule(new ChromeDriverProvider());
+	@Rule
+	public WebDriverExecutorRule webDriverExecutorRule = new WebDriverExecutorRule(new ChromeDriverProvider());
 
-    @Rule
-    public WebFactRule onCodecentricRule = new WebFactRule(
-            "I am on \"${url}\"",
-            onPage(pageVar("url"))
-    );
+	@Rule
+	public WebFactRule onCodecentricRule = new WebFactRule(
+			"I am on \"${url}\"",
+			onPage(pageVar("url"))
+	);
 
-    @Rule
-    public WebStepRule searchCodecentricRule = new WebStepRule(
-            "I type ${text} into the input field with the name ${name}",
-            type(charSequenceVar("text")).into(element(By.ByName.class, "name"))
-    );
+	@Rule
+	public WebStepRule searchCodecentricRule = new WebStepRule(
+			"I type ${text} into the input field with the name ${name}",
+			type(charSequenceVar("text")).into(element(By.ByName.class, "name"))
+	);
 
-    @Rule
-    public WebStepRule submitSearchRule = new WebStepRule(
-            "I submit the input field with the name ${name}", submit(element(By.ByName.class, "name"))
-    );
+	@Rule
+	public WebStepRule submitSearchRule = new WebStepRule(
+			"I submit the input field with the name ${name}", submit(element(By.ByName.class, "name"))
+	);
 
-    @Rule
-    public WebResultRule seeCodecentricOnPageRule = new WebResultRule(
-            "I see \"${text}\" on the page", see(stringVar("text"))
-    );
+	@Rule
+	public WebResultRule seeCodecentricOnPageRule = new WebResultRule(
+			"I see \"${text}\" on the page", see(stringVar("text"))
+	);
 
-    @Rule
-    public WebFactRule preparedFactRule = new WebFactRule(
-            "Open ${url}, type ${text} into ${name} and expect the same text.",
-            given(onPage(pageVar("url")))
-                    .when(type(charSequenceVar("text")).into(element(By.ByName.class, "name")))
-                    .andWhen(submit(element(By.ByName.class, "name")))
-                    .then(see(stringVar("text")))
-                    .asFact()
-    );
+	@Rule
+	public WebFactRule preparedFactRule = new WebFactRule(
+			"Open ${url}, type ${text} into ${name} and expect the same text.",
+			given(onPage(pageVar("url")))
+					.when(type(charSequenceVar("text")).into(element(By.ByName.class, "name")))
+					.andWhen(submit(element(By.ByName.class, "name")))
+					.then(see(stringVar("text")))
+					.asFact()
+	);
 
-    /**
-     * Reset the variable statement resolver so that statements registered within this test class are removed.
-     *
-     * Usually, this is not necessary.
-     */
-    @AfterClass
-    public static void tearDown() {
-        StatementResolverHolder.setStatementResolver(new VariableStatementResolver());
-    }
+	@Rule
+	public WebStepRule typeIntoElementByIdRule = new WebStepRule(
+			"I type ${text} into the element with the id ${name}",
+			type(charSequenceVar("text")).into(element(By.ById.class, "name"))
+	);
 
-    @Test
-    public void testRules() {
-        given("I am on \"http://www.codecentric.de\"")
-                .when("I type codecentric into the input field with the name s")
-                .andWhen("I submit the input field with the name s")
-                .then("I see \"codecentric\" on the page")
-                .end();
-    }
+	/**
+	 * Reset the variable statement resolver so that statements registered within this test class are removed.
+	 *
+	 * Usually, this is not necessary.
+	 */
+	@AfterClass
+	public static void tearDown() {
+		StatementResolverHolder.setStatementResolver(new VariableStatementResolver());
+	}
 
-    @Test
-    public void testPreparedRules() {
-        given("Open \"http://www.codecentric.de\", type codecentric into s and expect the same text.").end();
-    }
+	@Test
+	public void testRules() {
+		given("I am on \"http://www.codecentric.de\"")
+				.when("I type codecentric into the input field with the name s")
+				.andWhen("I submit the input field with the name s")
+				.then("I see \"codecentric\" on the page")
+				.end();
+	}
+
+	@Test
+	public void testPreparedRules() {
+		given("Open \"http://www.codecentric.de\", type codecentric into s and expect the same text.").end();
+	}
+
+	@Test
+	public void testReuseRule() {
+		given(onPage(url("https://www.codecentric.de/wp-login.php")))
+				.when(waitFor(By.id("user_login")))
+				.andWhen("I type username into the element with the id \"user_login\"")
+				.andWhen("I type password into the element with the id \"user_pass\"")
+				.andWhen(click(By.id("wp-submit")))
+				.then(see(By.id("login_error")))
+				.end();
+	}
 }
